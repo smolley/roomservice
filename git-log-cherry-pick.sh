80aada5825b33743d7b256ce1907c01908af6745
Brian <train.runaway@gmail.com>
2018-06-03 18:14:32 -0400

    update logging printing for git-log-cherry-pick

--- git-log-cherry-pick.sh
+++ git-log-cherry-pick.sh
@@ -1,2 +1,2 @@
  #!/bin/sh
- git log --left-right --graph --cherry-pick --oneline lineage-15.1...oreo-rr
+ git log --after="2018-04-01T16:36:00-07:00" --left-right --graph --date=local --cherry-pick --pretty=format:"%Cblue%h%Creset %s %Cgreen%an%Creset %ad %n" lineage-15.1...oreo-rr
